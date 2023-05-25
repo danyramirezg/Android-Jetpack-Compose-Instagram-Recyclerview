0a9be59801047210818150e3594bf151690d2a76
danyramirezg <danyramirezg@gmail.com>
2023-05-25 16:04:27 -0500

    Add Lazy vertical grid (recycler view)

--- app/src/main/java/com/example/androidjetpackcomposerecyclerview/ui/theme/Recyclerview.kt
+++ app/src/main/java/com/example/androidjetpackcomposerecyclerview/ui/theme/Recyclerview.kt
@@ -3,13 +3,16 @@ package com.example.androidjetpackcomposerecyclerview.ui.theme
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -28,49 +31,75 @@ import com.example.androidjetpackcomposerecyclerview.R
 import com.example.androidjetpackcomposerecyclerview.model.Countries
 
 @Composable
-fun CountriesView() {
+fun CountriesHorizontalRecyclerView() {
     val context = LocalContext.current
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
 
+    LazyColumn(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
         items(getCountriesFlag()) { country ->
-            ItemCountry(country = country, onItemSelected = {
+            ItemCountry(country = country) {
                 Toast.makeText(context, it.CountryName, Toast.LENGTH_SHORT).show()
-            })
+            }
         }
     }
 }
 
 @Composable
+fun CountriesGridRecyclerView() {
+    val context = LocalContext.current
+
+    LazyVerticalGrid(GridCells.Fixed(2), content = {
+        items(getCountriesFlag()) { country ->
+            ItemCountry(country = country)
+            { Toast.makeText(context, it.Continent, Toast.LENGTH_SHORT).show() }
+        }
+    })
+}
+
+@Composable
 fun ItemCountry(country: Countries, onItemSelected: (Countries) -> Unit) {
-    Card(border = BorderStroke(2.dp, Color.White), modifier = Modifier.width(200.dp)) {
-        Column() {
+    Card(border = BorderStroke(2.dp, Color.White),
+        modifier = Modifier
+            .width(200.dp)
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .clickable { onItemSelected(country) }) {
+        Column {
             Image(
                 painter = painterResource(id = country.photoFlag),
                 contentDescription = "Countries flag",
-                modifier = Modifier.fillMaxWidth().height(150.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
                 contentScale = ContentScale.Crop
             )
             Text(
                 text = country.CountryName,
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 fontFamily = FontFamily.Cursive,
-                fontWeight = FontWeight.ExtraBold
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 18.sp,
             )
             Text(
                 text = country.Capital,
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 fontFamily = FontFamily.Cursive,
-                fontSize = 12.sp
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
             Text(
                 text = country.Continent,
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(16.dp),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
                 fontFamily = FontFamily.Cursive,
-                fontSize = 10.sp
+                fontWeight = FontWeight.Black,
+                fontSize = 14.sp
             )
         }
     }
-
 }
 
 fun getCountriesFlag(): List<Countries> {
@@ -80,10 +109,17 @@ fun getCountriesFlag(): List<Countries> {
         Countries("France", "Paris", "Europe", R.drawable.france_flag),
         Countries("Italy", "Rome", "Europe", R.drawable.italy_flag),
         Countries("Spain", "Madrid", "Europe", R.drawable.spain_flag),
-        Countries("Ukraine", "Kiev", "Europe", R.drawable.ukraine_flag),
-        Countries("Germany", "Madrid", "Europe", R.drawable.germany_flag),
-        Countries("Ukraine", "Kiev", "Europe", R.drawable.ukraine_flag),
-        Countries("Switzerland", "-", "Europe", R.drawable.switzerland_flag),
-        Countries("United States", "Washington D.C.", "North America", R.drawable.unitates_states_flag),
+        Countries("Germany", "Berlin", "Europe", R.drawable.germany_flag),
+        Countries("Ukraine", "Kyiv", "Europe", R.drawable.ukraine_flag),
+        Countries("Switzerland", "Bern", "Europe", R.drawable.switzerland_flag),
+        Countries(
+            "United States",
+            "Washington, D.C.",
+            "North America",
+            R.drawable.unitates_states_flag
+        ),
+        Countries("Uruguay", "Montevideo", "South America", R.drawable.uruguay_flag),
+        Countries("Liechtenstein", "Vaduz", "Europe", R.drawable.liechestein_flag),
+        Countries("Ecuador", "Quito", "South America", R.drawable.ecuador_flag),
     )
 }
